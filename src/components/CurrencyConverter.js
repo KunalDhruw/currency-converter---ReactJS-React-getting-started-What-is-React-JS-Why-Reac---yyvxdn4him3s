936447925879b6a87d719d1fe142ae4d9c358679
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-12 22:55:15 +0530

    Update CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
-export default function CurrencyConverter() {
-    return (
-        <div>
-            
-        </div>
-    )
+function CurrencyConverter(props) {
+  return (
+    <div className="conveter_container">
+      <p>
+        {props.fromAmount} {props.firstInput} is eqauls to
+      </p>
+      <h3>
+        {props.toAmount} {props.secondInput}
+      </h3>
+      <div>
+        
+        <select value={props.firstInput} onChange={props.handleFromCurreny}>
+          {props.data.map((rate) => {
+            return (
+              <option key={rate} value={rate}>
+                {rate}
+              </option>
+            );
+          })}
+        </select>
+        <input
+          type="number"
+          value={props.fromAmount}
+          onChange={props.onMoneyChangeFrom}
+          min="1"
+        />
+      </div>
+      <div>
+        
+        <select value={props.secondInput} onChange={props.handleToCurrency}>
+          {props.data.map((rate) => {
+            return (
+              <option key={rate} value={rate}>
+                {rate}
+              </option>
+            );
+          })}
+        </select>
+        <input
+          type="number"
+          value={props.toAmount}
+          onChange={props.onMoneyChangeTo}
+          min="1"
+        />
+      </div>
+    </div>
+  );
 }
+export default CurrencyConverter;
